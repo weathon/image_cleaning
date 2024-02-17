cd21698160c3e72a15ffa3024ebdad3e193f3477
Wayne <wguo6358@gmail.com>
2024-02-17 22:51:55 +0000

    finished

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,28 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
 import sqlite3
 import os 
 
 con = sqlite3.connect("database.db")
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/get_image_url")
 async def get_image():
     cur = con.cursor()
     res = cur.execute("SELECT filename FROM images WHERE accept IS NULL LIMIT 1")
-    return res.fetchone()
+    ans = res.fetchone()
+    if ans is None:
+        return "finished"
+    return ans
 
 @app.get("/reset_database")
 async def reset_database():
@@ -21,15 +33,19 @@ async def reset_database():
     con.commit()
     return res 
 
-@app.get("/submit")
+@app.post("/submit")
 async def submit(filename, decision):
     cur = con.cursor()
-    res = cur.execute("UPDATE images SET accept=? WHERE filename=?", (decisionm, filename))
+    res = cur.execute("UPDATE images SET accept=? WHERE filename=?", (decision, filename))
     con.commit()
     return res 
 
 @app.get("/view_table")
-async def view_table(filename, decision):
+async def view_table():
     cur = con.cursor()
     res = cur.execute("SELECT * FROM images")
     return res 
+
+@app.get("/img")
+async def img(filename):
+    return FileResponse("images/"+filename)
